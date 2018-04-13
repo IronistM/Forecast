7fbad496cb9f72cb6cebc51cca0cc5aff6ecbc34
gkourogiorgas <g.kourogiorgas@gmail.com>
2018-04-13 13:07:02 +0100

    Update Models.R

--- Models.R
+++ Models.R
@@ -4,21 +4,20 @@ library(readxl)
 library(forecast)
 library(dplyr)
 
-data<-read_excel("Time Series/Items per day Online Global.xlsx", col_types = c("date", "numeric", "numeric", "numeric"))
-names(data)=c("Date", "Items","ASP","Sent")
+data<-read_excel("Time Series/Items.xlsx", col_types = c("date", "numeric"))
 
 N = ceiling(0.75*nrow(data))
 items=pull(data,Items)
 
-ts_items = ts(items[1:N], frequency=7)
+ts_items = ts(items[1:N], frequency=12)
 plot(ts_items)
-test.items <- ts(c(rep(NA, N), items[(N+1):NROW(items)]), frequency=7)
+test.items <- ts(c(rep(NA, N), items[(N+1):NROW(items)]), frequency=12)
 
 ##ets
 m_ets = ets(ts_items)
 f_ets = forecast(m_ets, h=nrow(data)-N) # forecast to test set into the future
 
-plot(f_ets,xlim=c(80,120))
+plot(f_ets)
 lines(test.items)
 
 rmse_ets <- sqrt(mean((f_ets$mean - test.items)^2, na.rm = TRUE))
@@ -26,8 +25,8 @@ rmse_ets
 
 ## Auto Arima
 m_aa = auto.arima(ts_items)
-f_aa = forecast(m_aa, h=nrow(data)-N)
-plot(f_aa,xlim=c(80,120))
+f_aa = forecast(m_aa, h=nrow(data)-N+1)
+plot(f_aa)
 lines(test.items)
 
 rmse_aa <- sqrt(mean((f_aa$mean - test.items)^2, na.rm = TRUE))
@@ -36,7 +35,7 @@ rmse_aa
 ## TBATS
 m_tbats = tbats(ts_items)
 f_tbats = forecast(m_tbats, h=nrow(data)-N)
-plot(f_tbats,xlim=c(80,120))
+plot(f_tbats)
 lines(test.items)
 
 rmse_tbats <- sqrt(mean((f_tbats$mean - test.items)^2, na.rm = TRUE))
