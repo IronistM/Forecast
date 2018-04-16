d986a172ef0d1fe528575f35e144e839796e697a
gkourogiorgas <g.kourogiorgas@gmail.com>
2018-04-16 15:45:35 +0100

    Update Models-function.R
    Some improvements!

--- Models-function.R
+++ Models-function.R
@@ -1,3 +1,5 @@
+## https://www.dataiku.com/learn/guide/code/r/time_series.html
+
 library(readxl)
 library(forecast)
 library(dplyr)
@@ -6,6 +8,7 @@ data<-read_excel("Time Series/Items.xlsx", col_types = c("date", "numeric"))
 plot(data[,2])
 
 models=c("meanf","naive","snaive","rwf","croston","stlf","ses","holt","hw","splinef","thetaf","ets","auto.arima","tbats")
+
 gkuniforecast = function(data, Np, Ncolumn, tsfreq, model) {
   ## Preparation
   N = ceiling(Np*nrow(data))
@@ -15,6 +18,8 @@ gkuniforecast = function(data, Np, Ncolumn, tsfreq, model) {
   
   ## Models
   str1=paste0("m_",model," = ",model,"(train.x)")
+  if (Np=1){str2=paste0("f_",model," = forecast(m_",model,", h=6")
+  } else {str2=paste0("f_",model," = forecast(m_",model,", h=NROW(x)-N)")}
   str2=paste0("f_",model," = forecast(m_",model,", h=NROW(x)-N)")
   str3=paste0("plot(f_",model,")")
   str4="lines(test.x)"
@@ -31,4 +36,12 @@ names(rmse)=models
 barplot(rmse,
         col="light blue",
         ylab="RMSE",
-        las=2)
+        las=2, main=paste0("Min RMSE is ",ceiling(min(rmse))," from model ",names(rmse[which.min(rmse)])))
+
+## Take model with smaller RMSE - in this case stlf
+NextMonth=forecast(stlf(ts(data[,2],frequency=12)),h=1)
+
+## Show min, max and mean prediction for next month
+X=ceiling(c(NextMonth$mean,NextMonth$upper[2],NextMonth$lower[2]))
+boxplot(X, horizontal = TRUE, axes = FALSE, staplewex = 1)
+text(x=fivenum(X), labels =fivenum(X), y=1.25)
