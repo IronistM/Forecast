438be6aa1889175b6f5e70e2c4c005c9d2b99b84
gkourogiorgas <g.kourogiorgas@gmail.com>
2018-04-16 12:41:05 +0100

    Create Models-function.R
    Put all models in a list and then with lapply I run all of them.
    Then I collect and plot RMSE.

---
+++ Models-function.R
@@ -0,0 +1,34 @@
+library(readxl)
+library(forecast)
+library(dplyr)
+
+data<-read_excel("Time Series/Items.xlsx", col_types = c("date", "numeric"))
+plot(data[,2])
+
+models=c("meanf","naive","snaive","rwf","croston","stlf","ses","holt","hw","splinef","thetaf","ets","auto.arima","tbats")
+gkuniforecast = function(data, Np, Ncolumn, tsfreq, model) {
+  ## Preparation
+  N = ceiling(Np*nrow(data))
+  x=pull(data,Ncolumn)
+  train.x = ts(x[1:N], frequency=tsfreq)
+  test.x <- ts(c(rep(NA, N), x[(N+1):NROW(x)]), frequency=tsfreq)
+  
+  ## Models
+  str1=paste0("m_",model," = ",model,"(train.x)")
+  str2=paste0("f_",model," = forecast(m_",model,", h=NROW(x)-N)")
+  str3=paste0("plot(f_",model,")")
+  str4="lines(test.x)"
+  str5=paste0("acc=accuracy(f_",model,",test.x)")
+  str=paste0(str1,";",str2,";",str3,";",str4,";",str5)
+  eval(parse(text=str))
+  return(acc)
+  }
+acc = lapply(models, gkuniforecast, data=data, Np=0.75, Ncolumn=2,tsfreq=12)
+names(acc)=models
+rmse=unlist(lapply(models,function(x) {acc[[x]]["Test set", "RMSE"]}))
+names(rmse)=models
+##Results
+barplot(rmse,
+        col="light blue",
+        ylab="RMSE",
+        las=2)
